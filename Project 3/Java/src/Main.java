44c4e48bba6bb71eaa0add0120041471fc37ef33
Noah-Wagner <noah@noah-wagner.com>
2017-04-08 09:22:10 -0400

    Fixed line separators

--- Project 3/Java/src/Main.java
+++ Project 3/Java/src/Main.java
@@ -1,1 +1,38 @@
-import javax.imageio.ImageIO;import java.awt.*;import java.awt.image.BufferedImage;import java.io.*;import java.util.Scanner;public class Main {	//TODO: Stretch goal, add service provider implementation	public static void main(String[] args) {		if (args.length != 3) {			System.out.println("USAGE: java seamcarve <file_name> s1 s2");			return;		}		String fileName = args[0];		int vertCount = Integer.parseInt(args[1]);		int horCount = Integer.parseInt(args[2]);		File file = new File(fileName);		try {			PMGImage image = PMGImage.createFromFile(file);			SeamCarver.seamCarve(image, vertCount, 0);			image.writeToFileName(getFileWithoutExtension(file.getName()) + "_processed.pgm");		} catch (IOException e) {			e.printStackTrace();		}	}	static String getFileWithoutExtension(String fName) {		return fName.substring(0, fName.lastIndexOf('.'));	}}
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+	//TODO: Stretch goal, add service provider implementation
+
+	public static void main(String[] args) {
+
+		if (args.length != 3) {
+			System.out.println("USAGE: java seamcarve <file_name> s1 s2");
+			return;
+		}
+		
+
+		String fileName = args[0];
+		int vertCount = Integer.parseInt(args[1]);
+		int horCount = Integer.parseInt(args[2]);
+
+		File file = new File(fileName);
+
+		try {
+			PMGImage image = PMGImage.createFromFile(file);
+			SeamCarver.seamCarve(image, vertCount, 0);
+			image.writeToFileName(getFileWithoutExtension(file.getName()) + "_processed.pgm");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	static String getFileWithoutExtension(String fName) {
+		return fName.substring(0, fName.lastIndexOf('.'));
+	}
+
+}
